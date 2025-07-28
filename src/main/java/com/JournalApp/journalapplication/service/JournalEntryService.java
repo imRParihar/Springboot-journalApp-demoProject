164a2c643437769b8fc14d5eb8e15f3b61d166bb
imRParihar <ravi.parihar@neuron7.ai>
2025-07-28 13:43:03 +0530

    list to *

--- src/main/java/com/JournalApp/journalapplication/service/JournalEntryService.java
+++ src/main/java/com/JournalApp/journalapplication/service/JournalEntryService.java
@@ -6,7 +6,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
 
 @Component
 public class JournalEntryService {
